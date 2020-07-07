0b04ef4ddd857bb672743fbe2d335a52b2a9625c
SaadMTSA <saadmtsa@gmail.com>
2020-07-07 13:48:25 +0300

    Add uniform, int_uniform, log_uniform to param distributions

--- optimyze/optimizers.py
+++ optimyze/optimizers.py
@@ -39,22 +39,29 @@ class LatinHypercubesCV:
             if isinstance(v, list):
                 for i in self.hp_set[0] * len(v):
                     adjusted_hp_set[-1].append(v[int(i)])
-            elif isinstance(v, tuple):
-                if 2 <= len(v) <= 3:
-                    possibilities = np.arange(*v)
-                    for i in (
-                        self.hp_set[0] * (v[1] - v[0]) / (1 if len(v) == 2 else v[2])
-                    ):
-                        adjusted_hp_set[-1].append(possibilities[int(i)])
-                else:
-                    raise ValueError(
-                        "Param value of type tuple should be of "
-                        "length 2 or 3 (start, end[, step])"
+            elif isinstance(v, tuple) and len(v) == 3:
+                if v[0] == "uniform":
+                    adjusted_hp_set[-1] = [i for i in self.hp_set[0] * (v[2] - v[1])]
+                elif v[0] == "log_uniform":
+                    possibilities = np.logspace(
+                        np.log2(v[1]), np.log2(v[2]), self.n_iter + 1, base=2
                     )
+                    intervals = (self.hp_set[0] * self.n_iter).astype(np.int64)
+                    for i in self.hp_set[0]:
+                        interval = int(i * self.n_iter)
+                        adjusted_hp_set[-1].append(
+                            (possibilities[interval + 1] - possibilities[interval]) * i
+                            + possibilities[interval]
+                        )
+                elif v[0] == "int_uniform":
+                    adjusted_hp_set[-1] = [
+                        int(i) if i >= v[1] else v[1] for i in self.hp_set[0] * (v[2] - v[1])
+                    ]
+
             else:
                 raise TypeError(
                     f"Cannot use {type(v)} ({v}) as param grid value. "
-                    "It should be a list or a tuple."
+                    "It should be a list or a tuple of length 3."
                 )
             self.hp_set = self.hp_set[1:]
 
@@ -72,7 +79,7 @@ class LatinHypercubesCV:
                     cv_results[mean_str].append(mean)
                 else:
                     cv_results[mean_str] = [mean]
-                
+
                 if std_str in cv_results:
                     cv_results[std_str].append(std)
                 else:
@@ -82,11 +89,14 @@ class LatinHypercubesCV:
             cv_results[i] = np.array(cv_results[i])
 
         self.cv_results_.update(cv_results)
-        
+        print(self.scores)
+        print(self.cv_results_)
+        print(self.hp_set)
+
     def fit(self, X, y, groups=None, **fit_params):
-    
+
         self.cv_results_ = {"params": []}
-        
+
         self.hp_set = lhs(len(self.param_distributions), self.n_iter).T
         self._calculate_intervals()
         scores = []
@@ -95,10 +105,10 @@ class LatinHypercubesCV:
             current_params = {}
             for j_idx, j in enumerate(self.param_distributions.keys()):
                 current_params[j] = self.hp_set[j_idx][i]
-            
+
             self.estimator.set_params(**current_params)
             self.cv_results_["params"].append(current_params)
-            
+
             scores.append(
                 cross_validate(
                     self.estimator,
